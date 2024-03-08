0e7ed24ee420ee02341f3b402e2b64ff8e14f405
Huzaifa <157941777+HuzaifaRehanMalik@users.noreply.github.com>
2024-03-08 14:49:40 +0500

    Add files via upload

---
+++ day7.js
@@ -0,0 +1,13 @@
+// Dinner Guests: Use one of your programs from Exercises 14 through 18 to print a message indicating the number of people you are inviting to dinner.
+var guestList = ["Ali", "Huzaifa", "Alyan", "Rehan"];
+console.log("Total number of guest in guest list is ".concat(guestList.length));
+// Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
+var cities = ["Karachi", "Lahore", "Islamabad"];
+console.log("I want to visite ", cities);
+// Think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+var student = {
+    name: "Huzaifa",
+    class: 10,
+    grade: "A+"
+};
+console.log("My name is ".concat(student.name, ".I study in class ").concat(student.class, ".I have pass my exam with grade ").concat(student.grade, "  "));
