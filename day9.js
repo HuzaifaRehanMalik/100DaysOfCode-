b23f9a6a4866886c50af58bdbaa091e6e34db73f
Huzaifa <157941777+HuzaifaRehanMalik@users.noreply.github.com>
2024-03-10 10:30:01 +0500

    Add files via upload

---
+++ day9.js
@@ -0,0 +1,24 @@
+// Alien Colors #1: Imagine an alien was just shot down in a game. Assign 'green', 'yellow', or 'red' to a variable called alien_color.
+var alien_color = "green";
+if (alien_color == "green") {
+    console.log("You have 5 points");
+}
+// Alien Colors #2: Choose a color for an alien, then write an if-else chain.
+var alienColor = "red";
+if (alienColor == "green") {
+    console.log("You have 5 points");
+}
+else {
+    console.log("You have 10 points");
+}
+// Alien Colors #3: Convert your if-else chain to handle three colors: green, yellow, red.
+var AlienColor = "yellow";
+if (AlienColor == "green") {
+    console.log("You have 10 points");
+}
+else if (AlienColor == "yellow") {
+    console.log("You have 15 points");
+}
+else {
+    console.log("You have 20 points");
+}
